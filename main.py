d23a1450699575cf47ce2cee9eea619f7d35ae77
alekssokolov164 <alekssokolov164@gmail.com>
2024-06-05 22:26:39 +0500

    7

--- main.py
+++ main.py
@@ -10,6 +10,28 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+@app.route('/index1')
+def index1():
+    return render_template('index1.html')
+
+@app.route('/index2')
+def index2():
+    return render_template('index2.html')
+
+@app.route('/index3')
+def index3():
+    return render_template('index3.html')
+
+@app.route('/index4')
+def index4():
+    return render_template('index4.html')
+@app.route('/index5')
+def index5():
+    return render_template('index5.html')
+
+@app.route('/index6')
+def index6():
+    return render_template('index6.html')
 
 @app.route('/about')
 def about():
