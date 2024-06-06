9bd665ac29d1dc588f7f7fc129cd913529f9a300
alekssokolov164 <alekssokolov164@gmail.com>
2024-06-06 11:07:54 +0500

    8

--- main.py
+++ main.py
@@ -32,7 +32,20 @@ def index5():
 @app.route('/index6')
 def index6():
     return render_template('index6.html')
+@app.route('/index7')
+def index7():
+    return render_template('index7.html')
 
+@app.route('/index8')
+def index8():
+    return render_template('index8.html')
+
+@app.route('/index9')
+def index9():
+    return render_template('index9.html')
+@app.route('/index10')
+def index10():
+    return render_template('index10.html')
 @app.route('/about')
 def about():
     return render_template('about.html')
