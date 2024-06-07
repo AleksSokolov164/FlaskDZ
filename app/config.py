1902971acec30105806ae10b7e77002c78697dd3
alekssokolov164 <alekssokolov164@gmail.com>
2024-06-07 14:14:06 +0500

    9

---
+++ app/config.py
@@ -0,0 +1,4 @@
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///newflask.db'
+    SECRET_KEY = 'jhgfjiunmbgfhglk9hlh6767khghckil90'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import  Flask, render_template
+from flask import Flask, render_template, url_for, request
 from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_ARI'] = 'sqlite:///newflask.db'
+#
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///newflask.db'
 # db = SQLAlchemy(app)
 
 
